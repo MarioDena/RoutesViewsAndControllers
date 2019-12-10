58ed6e89fa5a323f4a3f7e09c3026c34f506eeb9
MarioDena <enrique.dn@outlook.com>
2019-12-10 13:43:24 -0600

    Fixed Heroku

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,5 +1,5 @@
-require 'rest-client'
-  url = "http://localhost:3000/testing"
+#require 'rest-client'
+#  url = "http://localhost:3000/testing"
 
-  response = RestClient.get(url)
-  puts response
+#  response = RestClient.get(url)
+#  puts response
